7ebf8050d2f14370aaef6e57273f6220db270868
Amy Yuan <98059473+amyyuan2244@users.noreply.github.com>
2023-04-08 22:12:28 -0700

    Update and rename test_cnf.py to test_cyk.py

--- python/src/tests/test_cyk.py
+++ python/src/tests/test_cyk.py
@@ -1,26 +1,26 @@
 import unittest
 from gradescope_utils.autograder_utils.decorators import weight, visibility, number
-from parse import cnf
+from parse import cyk
 
 
 class TestComplex(unittest.TestCase):
     def setUp(self):
-        self.cnf = cnf()
+        self.cyk = cyk()
 
     @weight(2)
     @visibility('after_due_date')
     @number("2.1")
     def test_eval_parens(self):
-        """cyk(R1, "S", "01", {})"""
-        val = self.cnf.eval("cyk(R1, "S", "01", {})")
+        """R1, "S", "01", {}"""
+        val = self.cyk.eval("(R1, "S", "01", {})")
         self.assertEqual(val, "(True, (’S’, (’A’, ’0’), (’B’, ’1’)))")
 
     @weight(2)
     @visibility('after_due_date')
     @number("2.2")
     def test_eval_precedence(self):
-        """cyk(R1, "S", "0011", {})"""
-        val = self.calc.eval("cyk(R1, "S", "0011", {})")
+        """R1, "S", "0011", {}"""
+        val = self.cyk.eval("(R1, "S", "0011", {})")
         self.assertEqual(val, "(True, (’S’, (’C’, (’A’, ’0’), (’S’, (’A’, ’0’), (’B’, ’1’))), (’B’, ’1’)))")
 
  
