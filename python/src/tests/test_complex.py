6e03c3fdd426233e3a2e2679721ef20680ea3db4
Amy Yuan <98059473+amyyuan2244@users.noreply.github.com>
2023-04-08 22:06:08 -0700

    Update test_complex.py

--- python/src/tests/test_complex.py
+++ python/src/tests/test_complex.py
@@ -1,38 +1,26 @@
 import unittest
 from gradescope_utils.autograder_utils.decorators import weight, visibility, number
-from calculator import Calculator
+from parse import cyk, cnf, parse
 
 
 class TestComplex(unittest.TestCase):
     def setUp(self):
-        self.calc = Calculator()
+        self.cnf = cnf()
 
     @weight(2)
     @visibility('after_due_date')
     @number("2.1")
     def test_eval_parens(self):
-        """Evaluate (1 + 1) * 4"""
-        val = self.calc.eval("(1 + 1) * 4")
-        self.assertEqual(val, 8)
+        """cyk(R1, "S", "01", {})"""
+        val = self.cnf.eval("cyk(R1, "S", "01", {})")
+        self.assertEqual(val, "(True, (’S’, (’A’, ’0’), (’B’, ’1’)))")
 
     @weight(2)
     @visibility('after_due_date')
     @number("2.2")
     def test_eval_precedence(self):
-        """Evaluate 1 + 1 * 8"""
-        val = self.calc.eval("1 + 1 * 8")
-        self.assertEqual(val, 9)
+        """cyk(R1, "S", "0011", {})"""
+        val = self.calc.eval("cyk(R1, "S", "0011", {})")
+        self.assertEqual(val, "(True, (’S’, (’C’, (’A’, ’0’), (’S’, (’A’, ’0’), (’B’, ’1’))), (’B’, ’1’)))")
 
-    @weight(2)
-    @number("2.3")
-    def test_eval_mul_div(self):
-        """Evaluate 8 / 4 * 2"""
-        val = self.calc.eval("8 / 4 * 2")
-        self.assertEqual(val, 4)
-
-    @weight(2)
-    @number("2.4")
-    def test_eval_negative_number(self):
-        """Evaluate -2 + 6"""
-        val = self.calc.eval("-2 + 6")
-        self.assertEqual(val, 4)
+ 
